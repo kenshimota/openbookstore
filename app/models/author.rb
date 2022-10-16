ef4116940d20ff4101b1fc40aaabb89be4aace88
Erik Mota Galindo <kenshimota@gmail.com>
2022-10-16 00:14:09 -0400

    convert letter to lower and check if there is a author with their name

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,5 @@
 class Author < ApplicationRecord
   validates :name, presence: true
-  validates :name, uniqueness: true
   validates :name, length: { minimum: 3 }
+  validates :name, uniqueness: { case_sensitive: false }
 end
