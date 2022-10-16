a385fdf716d55a6ff8f333793a2b1be7e38db877
Erik Mota Galindo <kenshimota@gmail.com>
2022-10-15 23:42:54 -0400

    create session and users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::API
+  before_action :configure_sign_up_params, if: :devise_controller?
+  include DeviseTokenAuth::Concerns::SetUserByToken
+
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :last_name, :password])
+  end
 end
